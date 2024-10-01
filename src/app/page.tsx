45601dfebea14a58e3d49a9226cfc20924718dde
Ali Nazery <ali.nazery@example.com>
2024-10-01 09:48:44 +0430

    refined 03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Hero from "@/Components/Hero/Hero";
 import Services from "@/Components/MyServices/Services";
 import Projects from "@/Components/Projects/Projects";
 import Testimonial from "@/Components/Testimonial/Testimonial";
+import "../styles/globals.css";
+
 
 export default function Home() {
   return (
