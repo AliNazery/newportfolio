1fb1b3ddeeeba631b990b79ca1b80da6f882f7dd
Ali Nazery <ali.nazery@example.com>
2024-10-02 10:44:17 +0430

    content update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: "class",
   content: [
+    "*/",
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx}", // If you have layouts in the "src" folder
